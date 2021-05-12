171d326061afabc549554d837af37ef9bf67c3fb
Sharon-B <sharonbagnall@gmail.com>
2021-05-12 15:11:47 +0000

    Add toggle for done status

--- todo/views.py
+++ todo/views.py
@@ -43,3 +43,11 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+# Toggle item
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
